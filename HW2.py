cbff652d5a06d9534d3860b39323b9fd1dd83587
ColeDavis99 <coledavis6499@gmail.com>
2021-02-08 15:08:18 -0600

    1 done

--- HW2.py
+++ HW2.py
@@ -2,14 +2,39 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-G = nx.readwrite.gml.read_gml("dolphins.gml")
-
 def drawGraph(g):
 	pos = nx.spring_layout(g)
 	plt.figure(figsize=(10,10))
 	nx.draw_networkx(g, pos=pos, with_labels=True)
 	plt.axis('off')
 	plt.show()
+
+#Read in Data from file
+DolphinGraph = nx.readwrite.gml.read_gml("dolphins.gml")	
+#drawGraph(DolphinGraph)
+
+#(1) Output Degree Stats
+def DegreeOutput(g):
+	#Init
+	for node in g:	
+		minDegree = g.degree[node]
+		maxDegree = g.degree[node]
+		degreeSum = 0
+		break
 	
 	
-drawGraph(G)
+	for node in g:
+		if(g.degree[node] < minDegree):
+			minDegree = g.degree[node]
+		if(g.degree[node] > maxDegree):
+			maxDegree = g.degree[node]
+			
+		degreeSum += g.degree[node]
+	
+	print("=============== #1 ===============")
+	print("Max Degree: ", maxDegree)
+	print("Min Degree: ", minDegree)
+	print("Average Degree: ", degreeSum/len(g.nodes))
+	
+		
+DegreeOutput(DolphinGraph)
